e604060906bd22e5fb7e9c177f95d82045400388
KoniSan28 <koni.san.28@gmail.com>
2018-07-11 22:48:52 +0900

    メッセージの改行とオルトキー+エンターキーで送信する機能の追加

--- client.js
+++ client.js
@@ -1,33 +1,27 @@
 var s = io.connect();                                               //サーバーと接続
-var msg_in = document.getElementById("msg_in");                     //メッセージの入力欄
 var msg_out = document.getElementById("msg_out");                   //メッセージの出力欄
+var msg_form = document.getElementById("msg_form");                 //メッセージの送信フォーム
+var msg_in = document.getElementById("msg_in");                     //メッセージの入力欄
+var msg_button = document.getElementById("msg_button");             //メッセージの送信ボタン
 
-//サーバから受け取るイベント
+//サーバーからのイベントを受信
 s.on("connect", function () {});                                    //接続時
 s.on("disconnect", function (client) {});                           //切断時
-s.on("S_to_C_message", function (data) {                            //受信時
+s.on("S_to_C_message", function (data) {                            //相手からのメッセージを受信
     addMessage(data.value, 1);
 });
 
-//送信して自分のメッセージを追加
-function sendMyMessage() {
-    addMessage(msg_in.value, 0);
-    sendBroadcast();
-}
-
-//クライアントからイベント送信（イベント名は自由に設定できます）
-//みんなに送信
+//自分からメッセージを送信
 function sendMessage() {
-    var msg = msg_in.value;                                         //取得
-    msg_in.value = null;                                            //空白にする
-    s.emit("C_to_S_message", {value:msg});                          //サーバへ送信
-}
-
-//自分以外に送信
-function sendBroadcast() {
-    var msg = msg_in.value;                                         //取得
-    msg_in.value = null;                                            //空白にする
-    s.emit("C_to_S_broadcast", {value:msg});                        //サーバへ送信
+    var msg = msg_in.value;
+    msg_in.value = null;
+    msg_out.style.height = null;
+    msg_form.style.height = null;
+    msg_in.style.height = null;
+    msg_button.style.height = null;
+    //s.emit("C_to_S_message", {value:msg});                          //全員に送信
+    s.emit("C_to_S_broadcast", {value:msg});                        //自分以外に送信
+    addMessage(msg, 0);
 }
 
 //メッセージを追加
@@ -42,6 +36,9 @@ function addMessage(value,who) {
         msg_block.className = "msg_block left";
         msg.className = "msg left";
     }
+    if (value.match(/\n$/g)) {
+        value += "\n";
+    }
     msg.innerText = value;
     msg_block.appendChild(msg);
     msg_out.appendChild(msg_block);                                 //追加
@@ -50,14 +47,40 @@ function addMessage(value,who) {
     }
 }
 
-//エンターキーが押されたら自分以外に送信
-msg_in.addEventListener("keypress", (e)=>{
-    if (e.keyCode == 13) {
-        e.preventDefault();
+//改行に合わせてmsg_formをリサイズ
+function nlResize() {
+    var nl_num = msg_in.value.split("\n").length - 1;               //改行の個数
+    if (nl_num <= 5 && document.body.clientWidth > 480) {
+        msg_out.style.height = "calc(100% - " + String(70 + 24 * nl_num) + "px)";
+        msg_form.style.height = String(70 + 24 * nl_num) + "px";
+        msg_in.style.height = String(40 + 24 * nl_num) + "px";
+        msg_button.style.height = String(40 + 24 * nl_num) + "px";
+    } else if (nl_num <= 5) {
+        msg_out.style.height = "calc(100% - " + String(44 + 20 * nl_num) + "px)";
+        msg_form.style.height = String(44 + 20 * nl_num) + "px";
+        msg_in.style.height = String(34 + 20 * nl_num) + "px";
+        msg_button.style.height = String(34 + 20 * nl_num) + "px";
+    } else if (document.body.clientWidth > 480) {
+        msg_out.style.height = "calc(100% - 190px)";
+        msg_form.style.height = "190px";
+        msg_in.style.height = "160px";
+        msg_button.style.height = "160px";
+    } else {
+        msg_out.style.height = "calc(100% - 144px)";
+        msg_form.style.height = "144px";
+        msg_in.style.height = "134px";
+        msg_button.style.height = "134px";
     }
-});
+}
+
+msg_in.addEventListener("input", nlResize);
+window.addEventListener("resize", nlResize);
+
+//オルトキー+エンターキーが押されたら自分以外に送信
 msg_in.addEventListener("keyup", (e)=>{
-    if (e.keyCode == 13) {
-        sendMyMessage();
+    if (e.altKey && e.keyCode == 13) {
+        sendMessage();
     }
 });
+
+msg_button.addEventListener("click", sendMessage);
